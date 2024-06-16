042ba39fb2cb6d9c426acbdfc69c773da758ebc3
will <piliwilliam0306@gmail.com>
2024-06-16 14:35:04 +0800

    add QNetwork

---
+++ utils/model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+
+class QNetwork(nn.Module):
+
+    def __init__(self, state_size, action_size, fc1_units=64, fc2_units=64):
+        super(QNetwork, self).__init__()
+        self.fc1 = nn.Linear(state_size, fc1_units)
+        self.fc2 = nn.Linear(fc1_units, fc2_units)
+        self.fc3 = nn.Linear(fc2_units, action_size)
+
+    def forward(self, state):
+        x = torch.relu(self.fc1(state))
+        x = torch.relu(self.fc2(x))
+        return self.fc3(x)
